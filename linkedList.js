b4a1b4ff577537474de26271c51c9b6867fb5ab9
Chris Campbell <christopherecampbell@gmail.com>
2023-04-12 11:12:54 -0600

    add insert method

--- linkedList.js
+++ linkedList.js
@@ -61,12 +61,49 @@ class LinkedList {
       this.legnth++
     }
   }
+
+  //method to insert a node at a specific index
+  insert(index, data) {
+    if(index < 0 || index > this.legnth) { //checks if index is out of bounds
+      return -1
+    } else if (index === 0) { 
+      this.prepend(data)
+    } else if (index === this.legnth) {
+      this.append(data)
+    } else {
+      //create new node
+      const newNode = new Node(data)
+      //initialize counter to itterate through the LinkedList
+      let current = this.head
+      let counter = 0
+      //itteration happens in this while loop, counter is less than index -1 because we want to insert the new node before that node in the LinkedList nothing is being mutated in the LinkedList yet, this just sets current to the proper node
+      while(counter < index -1) {
+        counter++
+        current = current.next
+        console.log(current)
+      }
+      //sets a temporary variable to hold the pointer of the node in the index that will be inserted
+      let temp = current.next
+      //points the current node's next property to the new node
+      current.next = newNode
+      //sets the pointer of the inserted node as the next node in the LinkedList
+      newNode.next = temp
+      //incriments the lenghth of the LinkedList
+      this.legnth++
+    }
+    return this
+  }
 }
 
 const list = new LinkedList()
-list.prepend('first node')
-list.prepend('new head')
-list.prepend('newest head')
-list.append('appended node')
+list.append(list.legnth)
+list.append(list.legnth)
+list.append(list.legnth)
+list.append(list.legnth)
+list.append(list.legnth)
+list.append(list.legnth)
+list.append(list.legnth)
+list.append(list.legnth)
+list.insert(5, 'this will be inserted at index 5')
 
-console.log(list.getProps())
+console.log(list.print())
