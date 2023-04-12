b61f69f5497717a3a627e641a98a41e755bfa8d1
Chris Campbell <christopherecampbell@gmail.com>
2023-04-12 10:37:05 -0600

    add prepend method

--- linkedList.js
+++ linkedList.js
@@ -7,5 +7,33 @@ class Node {
   }
 }
 
-const node = new Node(1)
-console.log(node)
+// LinkedList class
+
+class LinkedList {
+  constructor() {
+    this.legnth = 0
+    this.head = null
+    this.tail = null
+  }
+
+  //method to add a new node as the head of the LinkedList
+  prepend(data) {
+    const node = new Node(data)
+
+    if(!this.head) { //this refers to the LinkedList that the method is being called on
+      this.head = node
+      this.tail = node
+      this.legnth++
+    } else {
+      node.next = this.head
+      this.head = node
+      this.legnth++
+    }
+  }
+}
+
+const list = new LinkedList()
+list.prepend('first node')
+list.prepend('new head')
+list.prepend('newest head')
+console.log(list)
