d1a70ba5dbb6be0ef9a95978e6cc45589c6a5c2d
Chris Campbell <christopherecampbell@gmail.com>
2023-04-19 12:28:55 -0600

    add get method

--- hashTable.js
+++ hashTable.js
@@ -3,11 +3,58 @@ function hash(key) {
   //itterate through the key
   for(let i = 0; i < key.length; i++) {
     let char = key.charCodeAt(i)
-    console.log(char)
     hash = (hash << 5) - hash + char
     // hash |= 0
   }
   return hash
 }
 
-console.log(hash('This is a really really long keyy for test'))
+
+
+//set up HashTable class
+
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size)
+  }
+
+  hash(key) {
+    let hash = 0
+    for(let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i)  % this.data.length
+    }
+    return hash
+  }
+
+  set(key, value) {
+    const address = this.hash(key)
+    if (!this.data[address]) {
+      this.data[address] = []
+    }
+    this.data[address].push([key, value])
+    return this.data
+  }
+
+  get(key) {
+    const address = this.hash(key)
+    const bucket = this.data[address]
+    if(bucket) {
+      for(let i = 0; i < bucket.length; i++) {
+        if(bucket[i][0] === key) {
+          return bucket[i][1]
+        }
+      }
+    } else {
+      return undefined
+    }
+  }
+}
+
+const ht = new HashTable(20)
+
+ht.set("test", 1)
+ht.set("anetra", "is kqeen")
+ht.set("sahsa", 100)
+ht.set("sirhc", 99)
+
+console.log(ht.get("anetra"))
